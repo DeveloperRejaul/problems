393e8ba6b9fdafa3c13c4969c7e993b92043e16b
Rezaul Karim <rezaul@orangetoolz.com>
2025-01-08 20:24:40 +0600

    add

---
+++ print-triangle/js/index.js
@@ -0,0 +1,19 @@
+const n = +process.argv[2]
+
+// way 01
+console.time()
+const star = new Array(n).fill(' ');
+for (let index = 0; index < (n+1) ; index++) {
+    console.log(star.join(""));
+    star[n-index] = "*"
+    star[n+index] = "*"
+}
+console.timeEnd()
+
+// way 02
+// console.time()
+// for (let i = 0; i < n; i++) {
+//     const str = " ".repeat(n-i) + "*".repeat(i*2+1) + " ".repeat(n-i)
+//     console.log(str);
+// }
+// console.timeEnd()
