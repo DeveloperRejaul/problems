8178b0795ff6309cba9e3909398cf6113d218099
Rezaul Karim <rezaul@orangetoolz.com>
2025-01-09 10:25:43 +0600

    add

--- todo-project/swift/main.swift
+++ todo-project/swift/main.swift
@@ -14,6 +14,7 @@ students.append(Person(name: "Ramjan", age: 22, roll: 2))
 students.append(Person(name: "Rezaul Karim", age: 22, roll: 3))
 students.append(Person(name: "Rezaul Karim", age: 22, roll: 4))
 
+
 // remove
 students.remove(at: 2) // remove   ny index
 students.removeFirst()
