3c42fe2684033aee0e487c684e98967280637692
Rezaul Karim <rezaul@orangetoolz.com>
2025-01-09 18:52:09 +0600

    add

--- pattern/swift/triangle.swift
+++ pattern/swift/triangle.swift
@@ -14,3 +14,7 @@ for i in 0...n {
 
 
 // way 02 : using string
+for index in 0...n {
+ let str =  String(repeating: " ", count: n-index) + String(repeating: "*", count: (index*2+1)) +  String(repeating: " ", count: n-index);
+  print(str);
+}
