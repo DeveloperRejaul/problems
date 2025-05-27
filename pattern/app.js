41ca29438811a7a69ca0001e0d6a2e7e306fe2ab
Rezaul Karim <rezaul@orangetoolz.com>
2025-05-27 22:22:29 +0600

    add patter problems

--- pattern/app.js
+++ pattern/app.js
@@ -1,7 +1,48 @@
 (function main() {
-  patten1(4)
+  patten5(5)
 })()
 
+function patten5 (n) {
+    const totalRows = n * 2 - 1;
+    for (let row = 0; row <= totalRows; row++) {
+      let str = "";
+      const totalCols = row <= n ? row: totalRows - row+1;
+      for (let col = 1; col <= totalCols; col++) {
+        str +="*";
+      }
+      console.log(str);
+    }
+}
+
+function patten4 (n) {
+    for (let row = 1; row <= n; row++) {
+      let str = "";
+      for (let cul = 1; cul <= row; cul++) {
+        str += cul.toString()+" ";
+      }
+      console.log(str);
+    }
+}
+
+function patten3 (n) {
+    for (let row = 0; row < n; row++) {
+      let str = "";
+      for (let cul = 0; cul < (n-row); cul++) {
+        str +="*"
+      }
+      console.log(str);
+    }
+}
+
+function patten2 (n) {
+    for (let row = 0; row <= n; row++) {
+       let str = "";
+       for (let cul = 0; cul < row; cul++) {
+            str +="*"
+        }
+        console.log(str);
+    }
+}
 
 function patten1 (n) {
     for (let row = 0; row <= n; row++) {
